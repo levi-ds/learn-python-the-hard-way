5df541e73fc6cf9e9922da7a14c96fd089d9e2b3
levi-ds <tshegofatsolevi@gmail.com>
2025-12-25 16:46:05 +0200

    lesson on argument vectors complete.

---
+++ argv_1.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv         #basically, give me every data before I start, contrary to input.
+
+print("The script is called:", script)      #script = argv[0]
+print("Your first variable is:", first)     #first = argv[1]
+print("Your second variable is:", second)   #second = argv[2]
+print("Your third variable is:", third)     #third = argv[3]
+
